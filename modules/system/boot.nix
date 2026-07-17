6ba0b7efcfeb338ac654f5f7c18e58412d54c966
lyh970817 <32429705+lyh970817@users.noreply.github.com>
2026-07-17 12:09:23 +0100

    Remove amdgpu initrd early-KMS workaround

--- modules/system/boot.nix
+++ modules/system/boot.nix
@@ -9,7 +9,4 @@
   # Bootloader
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
-  # Early KMS for AMD GPU to fix intermittent "HDMI not connected" issues
-  boot.initrd.kernelModules = [ "amdgpu" ];
 }
