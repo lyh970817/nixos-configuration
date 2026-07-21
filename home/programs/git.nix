c103757bd21f66bad39c9073dd3f8e5883ccfbb7
lyh970817 <32429705+lyh970817@users.noreply.github.com>
2026-07-21 18:10:40 +0800

    Configure Git author identity

--- home/programs/git.nix
+++ home/programs/git.nix
@@ -3,6 +3,8 @@
 {
   programs.git = {
     enable = true;
+    userName = "lyh970817";
+    userEmail = "32429705+lyh970817@users.noreply.github.com";
     ignores = [
       "**/.claude/settings.local.json"
     ];
