15f6eb49baf4103c940938cd3d1355207b2627e5
lyh970817 <32429705+lyh970817@users.noreply.github.com>
2026-07-30 17:36:16 +0800

    Restore CapsLock navigation layer

--- modules/services/keyd.nix
+++ modules/services/keyd.nix
@@ -12,7 +12,26 @@
     keyboards = {
       default = {
         ids = [ "*" ];
-        settings = { };
+        settings = {
+          main = {
+            # Activates the nav layer while CapsLock is held
+            capslock = "layer(nav)";
+          };
+          nav = {
+            # Vim-style navigation
+            h = "left";
+            j = "down";
+            k = "up";
+            l = "right";
+
+            # Optional: other useful shortcuts on this layer
+            u = "pageup";
+            d = "pagedown";
+            w = "C-right";
+            b = "C-left";
+            v = "S-right";
+          };
+        };
       };
     };
   };
