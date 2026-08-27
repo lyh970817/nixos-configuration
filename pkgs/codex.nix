bd40441609e665e709e96cf92224c53619648bf2
lyh970817 <32429705+lyh970817@users.noreply.github.com>
2026-08-28 03:26:34 +0800

    Bump codex to 0.150.1

--- pkgs/codex.nix
+++ pkgs/codex.nix
@@ -8,11 +8,11 @@
 
 stdenv.mkDerivation (finalAttrs: {
   pname = "codex";
-  version = "0.149.1";
+  version = "0.150.1";
 
   src = fetchzip {
     url = "https://registry.npmjs.org/@openai/codex/-/codex-${finalAttrs.version}-linux-x64.tgz";
-    hash = "sha256-Azsn8HRPBKaVK0qA0Pwu8eIkIdEzNrogMocd4Rdq3kI=";
+    hash = "sha256-hwpK17fTeCa+/smvRwk1KeYxKp+xyK2oEImckcpZsVo=";
   };
 
   dontBuild = true;
